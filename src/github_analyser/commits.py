4c128fa489aed3c28f1f98c31c0a812db8f864f3
mastoffel <martin.adam.stoffel@gmail.com>
2024-01-31 16:48:34 +0000

    add saving and docs

--- src/github_analyser/commits.py
+++ src/github_analyser/commits.py
@@ -1,86 +1,71 @@
-import os
-import requests
 import pandas as pd
+from github_analyser.utils import query_with_pagination
 
-def fetch_commits(repo_owner, repo_name, total_commits_to_fetch=20):
-    github_token = os.environ["GITHUB_TOKEN"]
-    headers = {"Authorization": f"Bearer {github_token}"}
 
+def fetch_commits(repo_owner: str, repo_name: str, total_commits_to_fetch=20, 
+                  save_csv=False, csv_path='commits.csv') -> pd.DataFrame:
+    """Fetch info about commits from a GitHub repository.
+    
+    Args:
+        repo_owner: The owner of the repository.
+        repo_name: The name of the repository.
+        total_commits_to_fetch: The total number of commits to fetch.
+        
+    Returns:
+        A pandas DataFrame with the following columns:
+            - message: The commit message.
+            - additions: The number of additions in the commit.
+            - deletions: The number of deletions in the commit.
+            - author: The author of the commit.
+            - date: The date of the commit.
+    """
+    
     query_template = """
-        query ($afterCursor: String) {
-            repository(owner: "%s", name: "%s") {
-                defaultBranchRef {
-                    target {
-                        ... on Commit {
-                            history(first: 10, after: $afterCursor) {
-                                edges {
-                                    node {
-                                        messageHeadline
-                                        author {
-                                            name
-                                            date
-                                        }
-                                        additions
-                                        deletions
+    query ($afterCursor: String) {
+        repository(owner: "%s", name: "%s") {
+            defaultBranchRef {
+                target {
+                    ... on Commit {
+                        history(first: 10, after: $afterCursor) {
+                            edges {
+                                node {
+                                    messageHeadline
+                                    author {
+                                        name
+                                        date
                                     }
+                                    additions
+                                    deletions
                                 }
-                                pageInfo {
-                                    endCursor
-                                    hasNextPage
-                                }
+                            }
+                            pageInfo {
+                                endCursor
+                                hasNextPage
                             }
                         }
                     }
                 }
             }
         }
-        """ % (repo_owner, repo_name)
+    }
+    """ % (repo_owner, repo_name)
 
-    url = "https://api.github.com/graphql"
-    hasNextPage = True
-    endCursor = None
-    commit_data = []
-    total_commits_to_fetch = total_commits_to_fetch
-    commits_fetched = 0
-
-    while hasNextPage:
-        payload = {
-            "query": query_template,
-            "variables": {
-                "afterCursor": endCursor,
-            },
-        }
-        response = requests.post(url, json=payload, headers=headers)
-        data = response.json()
-
-        commits = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]["edges"]
-        for commit in commits:
-            
-            if commits_fetched >= total_commits_to_fetch:
-                break
-            
-            commit_info = {
-                "messageHeadline": commit["node"]["messageHeadline"],
-                "authorName": commit["node"]["author"]["name"],
-                "authorDate": commit["node"]["author"]["date"],
-                "additions": commit["node"]["additions"],
-                "deletions": commit["node"]["deletions"]
-            }
-            commit_data.append(commit_info)
-            commits_fetched += 1
-
-        pageInfo = data["data"]["repository"]["defaultBranchRef"]["target"]["history"]["pageInfo"]
-        endCursor = pageInfo["endCursor"]
-        hasNextPage = pageInfo["hasNextPage"]
-
-        # Convert the list of commit data to a DataFrame
-        df = pd.DataFrame(commit_data)
+    responses = query_with_pagination(query_template, ["data", "repository", "defaultBranchRef", "target", "history"],
+                                      'afterCursor')
+  
+    nodes = []
+    for response in responses:
+        edges = response['data']['repository']['defaultBranchRef']['target']['history']['edges']
+        nodes.extend(edge['node'] for edge in edges)
+        if len(nodes) >= total_commits_to_fetch:
+            break
+    
+    df = pd.json_normalize(nodes, sep='_')
+    df.rename(columns={'messageHeadline': 'message',
+                       'author_name': 'author',
+                       'author_date': 'date'}, inplace=True)
+    
+    if save_csv:
+        df.to_csv(csv_path, index=False)
         
-        return df
-
-if __name__ == "__main__":
-    owner = 'alan-turing-institute'
-    repo = 'DTBase'
-    num_commits = 20
-    df = fetch_commits(owner, repo, num_commits)
-    print(df)
+    return df
