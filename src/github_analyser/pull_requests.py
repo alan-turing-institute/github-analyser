0ce9418579ae038244c6933d2ebc8de23335c72e
Rosie Wood <rwood@turing.ac.uk>
2024-01-31 16:37:20 +0000

    add repo name arg

--- src/github_analyser/pull_requests.py
+++ src/github_analyser/pull_requests.py
@@ -1,18 +1,12 @@
-import os
-
-import numpy as np
 import pandas as pd
 
 from github_analyser.utils import query_with_pagination
 
 
-def get_pull_requests_data():
-
-    # Define the headers for the request
-
+def get_pull_requests_data(repo_name):
     query = """
         query ($issues_end_cursor: String) {
-            repository(owner: "alan-turing-institute", name: "AutSPACEs") {
+            repository(owner: "alan-turing-institute", name: "%s") {
                 pullRequests(first: 10, after: $issues_end_cursor, states: OPEN) {
                     pageInfo {
                         endCursor
@@ -62,13 +56,15 @@ def get_pull_requests_data():
                 }
             }
     }
-    """
+    """ % (
+        repo_name
+    )
 
     return query_with_pagination(query, ["data", "repository", "pullRequests"])
 
 
-def get_pull_requests_df():
-    data = get_pull_requests_data()
+def get_pull_requests_df(repo_name):
+    data = get_pull_requests_data(repo_name=repo_name)
     data_nodes = [
         edge["node"]
         for datum in data
@@ -77,6 +73,7 @@ def get_pull_requests_df():
     df = pd.json_normalize(data_nodes)
     df["comments"] = df["comments.edges"].apply(get_authors)
     df["reviews"] = df["reviews.edges"].apply(get_authors)
+    df.drop(columns=["comments.edges", "reviews.edges"], inplace=True)
     return df
 
 
@@ -84,9 +81,4 @@ def get_authors(edge):
     authors = [i["node"]["author"]["login"] for i in edge]
     if len(authors) > 0:
         return (", ").join(authors)
-    return np.nan
-
-
-if __name__ == "__main__":
-    df = get_pull_requests_df()
-    df.to_csv("data/pull_requests.csv")
+    return ""
