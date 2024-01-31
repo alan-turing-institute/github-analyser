2cd74e6b789b7f6d31068dfb5db42b80bad346df
mastoffel <martin.adam.stoffel@gmail.com>
2024-01-31 17:00:23 +0000

    small doc change

--- src/github_analyser/commits.py
+++ src/github_analyser/commits.py
@@ -1,16 +1,22 @@
 import pandas as pd
+
 from github_analyser.utils import query_with_pagination
 
 
-def fetch_commits(repo_owner: str, repo_name: str, total_commits_to_fetch=20, 
-                  save_csv=False, csv_path='commits.csv') -> pd.DataFrame:
-    """Fetch info about commits from a GitHub repo.
-    
+def fetch_commits(
+    repo_owner: str,
+    repo_name: str,
+    total_commits_to_fetch=20,
+    save_csv=False,
+    csv_path="commits.csv",
+) -> pd.DataFrame:
+    """Fetch info about commits from a GitHub repository.
+
     Args:
         repo_owner: The owner of the repository.
         repo_name: The name of the repository.
         total_commits_to_fetch: The total number of commits to fetch.
-        
+
     Returns:
         A pandas DataFrame with the following columns:
             - message: The commit message.
@@ -19,53 +25,63 @@ def fetch_commits(repo_owner: str, repo_name: str, total_commits_to_fetch=20,
             - author: The author of the commit.
             - date: The date of the commit.
     """
-    
-    query_template = """
-    query ($afterCursor: String) {
-        repository(owner: "%s", name: "%s") {
-            defaultBranchRef {
-                target {
-                    ... on Commit {
-                        history(first: 10, after: $afterCursor) {
-                            edges {
-                                node {
+
+    query_template = f"""
+    query ($afterCursor: String) {{
+        repository(owner: "{repo_owner}", name: "{repo_name}") {{
+            defaultBranchRef {{
+                target {{
+                    ... on Commit {{
+                        history(first: 10, after: $afterCursor) {{
+                            edges {{
+                                node {{
                                     messageHeadline
-                                    author {
+                                    author {{
                                         name
                                         date
-                                    }
+                                    }}
                                     additions
                                     deletions
-                                }
-                            }
-                            pageInfo {
+                                }}
+                            }}
+                            pageInfo {{
                                 endCursor
                                 hasNextPage
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    """ % (repo_owner, repo_name)
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        }}
+    }}
+    """
 
-    responses = query_with_pagination(query_template, ["data", "repository", "defaultBranchRef", "target", "history"],
-                                      'afterCursor')
-  
+    responses = query_with_pagination(
+        query_template,
+        ["data", "repository", "defaultBranchRef", "target", "history"],
+        "afterCursor",
+    )
+
     nodes = []
     for response in responses:
-        edges = response['data']['repository']['defaultBranchRef']['target']['history']['edges']
-        nodes.extend(edge['node'] for edge in edges)
+        edges = response["data"]["repository"]["defaultBranchRef"]["target"]["history"][
+            "edges"
+        ]
+        nodes.extend(edge["node"] for edge in edges)
         if len(nodes) >= total_commits_to_fetch:
             break
-    
-    df = pd.json_normalize(nodes, sep='_')
-    df.rename(columns={'messageHeadline': 'message',
-                       'author_name': 'author',
-                       'author_date': 'date'}, inplace=True)
-    
+
+    df = pd.json_normalize(nodes, sep="_")
+    df.rename(
+        columns={
+            "messageHeadline": "message",
+            "author_name": "author",
+            "author_date": "date",
+        },
+        inplace=True,
+    )
+
     if save_csv:
         df.to_csv(csv_path, index=False)
-        
+
     return df
