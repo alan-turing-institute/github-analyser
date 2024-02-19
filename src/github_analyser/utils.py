776ec75fed00ed629612df39f4fab1f89fef385a
Markus Hauru <mhauru@turing.ac.uk>
2024-02-19 13:19:44 +0000

    Add a docstring

--- src/github_analyser/utils.py
+++ src/github_analyser/utils.py
@@ -84,5 +84,6 @@ def query_with_pagination(
 
 
 def camel_to_snake(name):
+    """Convert a camel case string to snake case."""
     name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
